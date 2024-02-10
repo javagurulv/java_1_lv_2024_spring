ecb14e409dce54a5808c5d136ab69fff4498679a
javagurulv <javagurulv@gmail.com>
2024-02-10 21:07:06 +0200

    lesson_11

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_11_interfaces/lessoncode/PremiumCalculator.java
@@ -0,0 +1,9 @@
+package lv.javaguru.java1.teacher.lesson_11_interfaces.lessoncode;
+
+import java.math.BigDecimal;
+
+public interface PremiumCalculator {
+
+    BigDecimal calculate(Policy policy);
+
+}
