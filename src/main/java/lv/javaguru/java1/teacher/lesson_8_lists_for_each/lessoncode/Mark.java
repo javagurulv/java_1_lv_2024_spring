97ee915294c189eea0f54b0296b78c7b93d875fc
javagurulv <javagurulv@gmail.com>
2024-02-10 20:39:57 +0200

    lesson_8

---
+++ src/main/java/lv/javaguru/java1/teacher/lesson_8_lists_for_each/lessoncode/Mark.java
@@ -0,0 +1,20 @@
+package lv.javaguru.java1.teacher.lesson_8_lists_for_each.lessoncode;
+
+class Mark {
+
+    private String subject;
+    private int mark;
+
+    public Mark(String subject, int mark) {
+        this.subject = subject;
+        this.mark = mark;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+}
