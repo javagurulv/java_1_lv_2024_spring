6cea1647ce740c1d1311f8842b9c150983ba5c20
paula strazdina <paulapaulinestrazdina@gmail.com>
2024-02-22 22:13:35 +0200

    Initial commit/lesson1_1

---
+++ src/main/java/lv/javaguru/java1/student_Paula_Strazdina/lesson_1/HelloWorld.java
@@ -0,0 +1,7 @@
+package lv.javaguru.java1.student_Paula_Strazdina.lesson_1;
+
+class HelloWorld {
+        public static void main(String[] args) {
+                System.out.println("Hello World!");
+        }
+}
