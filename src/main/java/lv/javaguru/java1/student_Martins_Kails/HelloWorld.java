7684c03258f53f27bbb0c0370483d4b748875317
Mārtiņš <martins.kails@inbox.lv>
2024-02-29 19:38:18 +0200

    Initial commit/lessons1_1

--- src/main/java/lv/javaguru/java1/student_Martins_Kails/HelloWorld.java
+++ src/main/java/lv/javaguru/java1/student_Martins_Kails/HelloWorld.java
@@ -2,6 +2,6 @@ import java.util.function.Function;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");}
+            System.out.println("HelloWorld");}
 
 }
