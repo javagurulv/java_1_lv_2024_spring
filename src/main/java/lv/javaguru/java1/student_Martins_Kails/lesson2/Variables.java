515f45a6bb39ec60c1209841d6cfa3f5f9199ac6
Mārtiņš <martins.kails@inbox.lv>
2024-02-29 22:20:28 +0200

    Initial commit/lessons2_variables

---
+++ src/main/java/lv/javaguru/java1/student_Martins_Kails/lesson2/Variables.java
@@ -0,0 +1,25 @@
+package lesson2;
+
+public class Variables {
+    public static void main(String[] args) {
+//        int age1;
+//        int age2 = 40;
+//
+//        short selfEsteem =  -6;
+//        double weight = 70.5;
+//        long heigt = 90;
+//
+////        System.out.println(age1);
+//        System.out.println(age2);
+//
+//        System.out.println(selfEsteem);
+//        System.out.println(weight);
+//        System.out.println(heigt);
+//
+            int a = 15;
+            int b = 30;
+            int results = a + b;
+
+            System.out.println(results);
+    }
+}
