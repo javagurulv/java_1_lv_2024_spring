c47142add503f899db80a2a3ed3e38d6e278c1fa
Mārtiņš <martins.kails@inbox.lv>
2024-02-29 22:17:53 +0200

    Initial commit/lessons2_variables

---
+++ src/main/java/lv/javaguru/java1/student_Martins_Kails/lesson2/twoVariablessum.java
@@ -0,0 +1,31 @@
+package lesson2;
+
+import java.util.Random;
+
+public class twoVariablessum {
+    public static void main(String[] args) {
+
+
+//        double a = 1.5;
+//        double b = 4.5;
+//
+//        double resultsum = a + b;
+//        double resultsub = a - b;
+//        double resultmult = a * b;
+//        double resultdiv = a / b;
+//
+//        System.out.println("resultsum:" + resultsum);
+//        System.out.println("resultsub:" + resultsub);
+//        System.out.println("resultmult:" + resultmult);
+//        System.out.println("resultdiv:" + resultdiv);
+
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(101);
+
+        System.out.println(randomNumber);
+
+    }
+
+
+    }
+
