e816df2a2c9e4d725a7d6fc86be1766d4074beef
karlis <karlis.podniex@gmail.com>
2024-02-29 17:22:10 +0100

    inititial lesson1

--- src/main/java/lv/javaguru/java1/student_karlis_podnieks/HelloWorld.java
+++ src/main/java/lv/javaguru/java1/student_karlis_podnieks/HelloWorld.java
@@ -3,5 +3,11 @@ package lv.javaguru.java1.student_karlis_podnieks;
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // skaitlu dalisana
+        System.out.println(50 / 3);
+        //skaitlu reizinasana
+        System.out.println(50 * 3);
+        //skaitlu saskaitisana
+        System.out.println(74 + 36);
     }
 }
