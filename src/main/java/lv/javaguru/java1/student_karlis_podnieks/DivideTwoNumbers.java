6869c025b3a74374a45d5d3f916e535867df2897
karlis <karlis.podniex@gmail.com>
2024-02-29 17:23:26 +0100

    inititial lesson1

--- src/main/java/lv/javaguru/java1/student_karlis_podnieks/DivideTwoNumbers.java
+++
@@ -1,8 +0,0 @@
-package lv.javaguru.java1.student_karlis_podnieks;
-
-public class DivideTwoNumbers {
-
-    public static void main(String[] args) {
-        System.out.println(50 / 3);
-    }
-}
--- src/main/java/lv/javaguru/java1/student_karlis_podnieks/MultipyTwoNumbers.java
+++
@@ -1,8 +0,0 @@
-package lv.javaguru.java1.student_karlis_podnieks;
-
-public class MultipyTwoNumbers {
-
-    public static void main(String[] args) {
-        System.out.println(50 * 3);
-    }
-}
